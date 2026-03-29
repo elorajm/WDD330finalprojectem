b09b84eb616734aed9f3a275881755281ea4f8a5
elorajm <eloraathias@gmail.com>
2026-03-29 17:40:23 -0500

    server

--- server/server.js
+++ server/server.js
@@ -52,9 +52,10 @@ const allowedOrigins = new Set([
     .split(',')
     .map(u => u.trim())
     .filter(Boolean),
-  'http://127.0.0.1:5500',   // Live Server default (local dev)
+  process.env.RENDER_EXTERNAL_URL,  // Render injects this automatically
+  'http://127.0.0.1:5500',
   'http://localhost:5500',
-]);
+].filter(Boolean));
 
 app.use(cors({
   origin: (origin, callback) => {
